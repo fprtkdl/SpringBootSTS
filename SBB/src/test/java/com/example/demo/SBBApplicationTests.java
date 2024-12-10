1d1cf52ae539e62536e3767958ef0e3893281353
fprtkdl <beatpunchbeef141211@gmail.com>
2024-12-10 17:11:29 +0900

    answer data save

--- SBB/src/test/java/com/example/demo/SBBApplicationTests.java
+++ SBB/src/test/java/com/example/demo/SBBApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
+import com.mysite.sbb.Answer;
+import com.mysite.sbb.AnswerRepository;
 import com.mysite.sbb.Question;
 import com.mysite.sbb.QuestionRepository;
 import com.mysite.sbb.SBBApplication;
@@ -22,14 +25,19 @@ class SBBApplicationTests
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+    @Autowired
+    private AnswerRepository answerRepository;
+	
 	@Test
 	void testJpa() {
-        assertEquals(2, this.questionRepository.count());
-        Optional<Question> oq = this.questionRepository.findById(1);
+        Optional<Question> oq = this.questionRepository.findById(2);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        this.questionRepository.delete(q);
-        assertEquals(1, this.questionRepository.count());
-		
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
 	}
 }
